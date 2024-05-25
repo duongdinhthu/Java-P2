668fd052c3fded1f26c0e29a6c02997a5495797a
Dương Thứ <thuddth2307004@fpt.edu.vn>
2024-05-25 09:10:30 +0700

    up

--- src/baithi/Application.java
+++ src/baithi/Application.java
@@ -13,30 +13,35 @@ public class Application {
     public static void main(String[] args) throws SQLException, IOException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, ParseException {
         View view = new View();
         ReadJson readJson = new ReadJson();
-        Scanner scanner = new Scanner(System.in);
+        while (true){
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Select choice:");
-        System.out.println("1. Get All Student and writer to JSON");
-        System.out.println("2. Read File Json");
-        System.out.println("3. Read File Json search name");
-        System.out.println("4. Read File Json search email");
-        System.out.println("0. Exit");
-        int choice = scanner.nextInt();
-        switch (choice){
-            case 1:
-                view.getAll();
-                break;
-            case 2:
-                readJson.readJson();
-                break;
-            case 3:
-                readJson.searchName();
-                break;
-            case 4:
-                readJson.searchEmail();
-            case 0:
-                System.exit(0);
-                break;
+            System.out.println("Select choice:");
+            System.out.println("1. Get All Student and writer to JSON");
+            System.out.println("2. Read File Json");
+            System.out.println("3. Read File Json search name");
+            System.out.println("4. Read File Json search email");
+            System.out.println("0. Exit");
+            int choice = scanner.nextInt();
+            switch (choice){
+                case 1:
+                    view.getAll();
+                    break;
+                case 2:
+                    readJson.readJson();
+                    break;
+                case 3:
+                    readJson.searchName();
+                    break;
+                case 4:
+                    readJson.searchEmail();
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("khong co lua con nay");
+                    break;
+            }
         }
     }
 }
